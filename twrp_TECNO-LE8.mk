d69280e15dfc0c6071053ddfd6ad820a575901c4
Alhadi1024 <117118314+Alhadi1024@users.noreply.github.com>
2023-08-07 20:38:49 +0600

    Update and rename omni_TECNO-LE8.mk to twrp_TECNO-LE8.mk
    Omni to twrp

--- twrp_TECNO-LE8.mk
+++ twrp_TECNO-LE8.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TECNO-LE8 device
 $(call inherit-product, device/tecno/TECNO-LE8/device.mk)
 
 PRODUCT_DEVICE := TECNO-LE8
-PRODUCT_NAME := omni_TECNO-LE8
+PRODUCT_NAME := twrp_TECNO-LE8
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO LE8
 PRODUCT_MANUFACTURER := tecno
