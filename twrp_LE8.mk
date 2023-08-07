1d6c1ec93b592a0c4414612ba7a7e91dd8968170
Alhadi1024 <117118314+Alhadi1024@users.noreply.github.com>
2023-08-08 00:23:52 +0600

    Update twrp_LE8.mk

--- twrp_LE8.mk
+++ twrp_LE8.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from TECNO-LE8 device
+# Inherit from LE8 device
 $(call inherit-product, device/tecno/LE8/device.mk)
 
 PRODUCT_DEVICE := LE8
@@ -26,4 +26,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-tecno
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vnd_le8_h771-user 11 RP1A.200720.011 78515 release-keys"
 
-BUILD_FINGERPRINT := TECNO/LE8-GL/LE8:11/RP1A.200720.011/220902V932:user/release-keys
+BUILD_FINGERPRINT := TECNO/LE8-GL/TECNO_LE8:11/RP1A.200720.011/220902V932:user/release-keys
